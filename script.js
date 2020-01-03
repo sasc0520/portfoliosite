1951decca328aaeba75ba19293d01fa127ac6f81
sasc0520 <sasc0520@stud.kea.dk>
2020-01-03 17:03:04 +0100

    menu styling ved scroll

---
+++ script.js
@@ -0,0 +1,55 @@
+const header = document.querySelector("header");
+const sectionOne = document.querySelector("#forside");
+
+const faders = document.querySelectorAll(".fade_in");
+const sliders = document.querySelectorAll(".slide_in");
+
+//sectionOneOptions funktion er her at fortælle hvilke specifikt punkt intersectionobserveren skal lytte efter
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+        entries,
+        sectionOneObserver
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                header.classList.add("nav_scrolled");
+            } else {
+                header.classList.remove("nav_scrolled");
+            }
+        });
+    },
+    sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+        entries,
+        appearOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add("appear");
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    },
+    appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
